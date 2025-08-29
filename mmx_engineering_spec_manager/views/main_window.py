bf73685ef0891d50343be9d46d9b52549c85a2e2
Dave Kane <davelkane@gmail.com>
2025-08-29 13:29:03 -0500

    feat: Add main window menu bar

--- mmx_engineering_spec_manager/views/main_window.py
+++ mmx_engineering_spec_manager/views/main_window.py
@@ -1,6 +1,12 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMenu, QMenuBar
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Spec Manager")
+
+        # Create the menu bar
+        self.menu_bar = self.menuBar()
+
+        # Add the 'File' menu
+        self.file_menu = self.menu_bar.addMenu("&File")
