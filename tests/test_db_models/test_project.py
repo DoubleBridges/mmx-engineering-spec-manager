ff3468d41f5c2a26d4dd2e4b7ce3f7ec7702c487
Dave Kane <davelkane@gmail.com>
2025-08-30 23:24:50 -0500

    feat: Added relationship between custom field and project

--- tests/test_db_models/test_project.py
+++ tests/test_db_models/test_project.py
@@ -1,4 +1,6 @@
 import pytest
+
+from mmx_engineering_spec_manager.db_models.custom_field import CustomField
 from mmx_engineering_spec_manager.db_models.project import Project
 
 def test_project_model_creation(db_session):
@@ -18,3 +20,27 @@ def test_project_model_creation(db_session):
     assert retrieved_project.number == "101"
     assert retrieved_project.name == "Test Project"
     assert retrieved_project.job_description == "A complete project example."
+
+def test_project_has_custom_fields(db_session):
+    """
+    Test that a project can have a custom field.
+    """
+    project = Project(
+        number="101",
+        name="Test Project",
+        job_description="A complete project example."
+    )
+    db_session.add(project)
+    db_session.commit()
+
+    custom_field = CustomField(
+        name="Contractor",
+        value="ABC Inc.",
+        project_id=project.id
+    )
+    db_session.add(custom_field)
+    db_session.commit()
+
+    assert len(project.custom_fields) == 1
+    assert project.custom_fields[0].name == "Contractor"
+    assert project.custom_fields[0].value == "ABC Inc."
