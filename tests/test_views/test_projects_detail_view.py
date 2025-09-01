ceceed63dfa8061d84f9abbc76c28af43e004750
Dave Kane <davelkane@gmail.com>
2025-09-01 06:00:36 -0500

    fix: added form and tabs test back

--- tests/test_views/test_projects_detail_view.py
+++ tests/test_views/test_projects_detail_view.py
@@ -4,6 +4,20 @@ from PySide6.QtWidgets import QFormLayout, QTabWidget
 
 from mmx_engineering_spec_manager.views.projects.projects_detail_view import ProjectsDetailView
 
+def test_projects_detail_view_has_form_and_tabs(qtbot):
+    """
+    Test that the ProjectsDetailView has a QFormLayout and a QTabWidget.
+    """
+    projects_detail_view = ProjectsDetailView()
+    qtbot.addWidget(projects_detail_view)
+
+    # Assert that a QFormLayout exists
+    form_layout = projects_detail_view.findChild(QFormLayout)
+    assert form_layout is not None
+
+    # Assert that a QTabWidget exists
+    tab_widget = projects_detail_view.findChild(QTabWidget)
+    assert tab_widget is not None
 
 def test_projects_detail_view_displays_project_data(qtbot):
     """
